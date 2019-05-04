566014ce43ccb956ff344981b045ff29ba4b4f0e
Frédérique Milc <frederique@milc.fr>
2019-05-04 22:43:54 +0200

    Give that component some look and feel 👓 May the 4th be with it
    Add bootstrap and update styles.
    Add displayedDomains to hold count of displayable results and
    countDisplayedDomains() to get the value.
    Add regex support.

--- src/app/filtered-select/filtered-select.component.ts
+++ src/app/filtered-select/filtered-select.component.ts
@@ -11,6 +11,7 @@ export class FilteredSelectComponent implements OnInit {
 
   domains = DOMAINS;
   filteredDomains = [];
+  displayedDomains: number;
   selectedDomain: Domain;
   query: string;
   private storageName = 'filtered-select-state';
@@ -28,6 +29,7 @@ export class FilteredSelectComponent implements OnInit {
     if(storage.filteredDomains && storage.filteredDomains !== '') {
       this.filteredDomains = storage.filteredDomains;
     }
+    this.countDisplayedDomains();
   }
 
   onSelect(domain: Domain): void {
@@ -39,18 +41,40 @@ export class FilteredSelectComponent implements OnInit {
     let hasMatch = false;
 
     this.filteredDomains = this.domains.map(domain => {
-      if(domain.name.includes(query)) {
+
+      let regmatch = null;
+
+      try {
+        const regxp = new RegExp(query); // query.replace(/[#-.]|[[-^]|[?|{}]/g, '\\$&')
+        regmatch = domain.name.match(regxp);
+      } catch(err) {}
+
+      if(domain.name.includes(query) || (regmatch !== null && regmatch.length > 0)) {
         domain.display = true;
         hasMatch = true;
       } else {
         domain.display = false;
       }
+
       return domain;
     });
+
+    this.countDisplayedDomains();
 
     if(hasMatch) {
       this.updateStorage();
     }
+  }
+
+  countDisplayedDomains(): void {
+    let count = 0;
+    this.filteredDomains.forEach(domain => {
+      if(domain.display) {
+        count++;
+      }
+      // return count;
+    });
+    this.displayedDomains = count;
   }
 
   getStorage(): any {
@@ -64,6 +88,7 @@ export class FilteredSelectComponent implements OnInit {
     return null;
   }
 
+  // TODO: cookie fallback
   updateStorage(): void {
     if (window.localStorage) {
       localStorage.setItem(this.storageName, JSON.stringify({query: this.query, filteredDomains: this.filteredDomains}));
